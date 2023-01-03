b50a5944c5353036411221341c547c2c3bff5b1b
Niral <niral.dhameliya@checker-solutions.com>
2023-01-03 16:22:04 +0530

    Added : back image

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -7,4 +7,5 @@ class AppImages {
   static final microSoft = _fullPath('microsoft.png');
   static final apple = _fullPath('apple.png');
   static final google = _fullPath('google.png');
+  static final back = _fullPath('back.svg');
 }
