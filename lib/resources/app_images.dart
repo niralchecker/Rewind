c635f4be1d5cb91c5a0f4a691605c8ccdc6ba05b
Niral <niral.dhameliya@checker-solutions.com>
2023-01-03 11:23:16 +0530

    Added : Login Screen Ui

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -4,5 +4,7 @@ class AppImages {
   static const _base = 'assets/images/';
 
   static String _fullPath(String name) => '$_base$name';
-  static final splashLogoImage = _fullPath('splash_logo.png');
+  static final microSoft = _fullPath('microsoft.png');
+  static final apple = _fullPath('apple.png');
+  static final google = _fullPath('google.png');
 }
--- lib/resources/app_texts.dart
+++ lib/resources/app_texts.dart
@@ -2,4 +2,13 @@ class AppTexts {
   const AppTexts._();
 
   static String next = 'Next';
+  static String microsoft = 'Microsoft';
+  static String appleId = 'Apple ID';
+  static String google = 'Google';
+  static String orContinueWith = 'or continue  with';
+  static String email = 'Email';
+  static String signUp = 'Sign Up';
+  static String signUpWith = 'Sign up with';
+  static String login = 'Log In';
+  static String alreadyAccount = 'Already have account? ';
 }
